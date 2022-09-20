76f1a89f0c1b14634fe30fedbe62a89619682bb5
remiNdong <ndongremi@aol.fr>
2022-09-20 18:12:43 +0200

    first commit

---
+++ PHONETASTIK/src/main/java/fr/phonetastik/PhonetastikApplication.java
@@ -0,0 +1,13 @@
+package fr.phonetastik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhonetastikApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhonetastikApplication.class, args);
+	}
+
+}
