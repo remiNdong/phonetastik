20f35fc215e05b7cae3248ba8b26caee4ef37b4c
remiNdong <ndongremi@aol.fr>
2022-10-04 20:30:09 +0200

    creation page acceuil

--- PHONETASTIK/src/main/java/fr/phonetastik/controller/AcceuilController.java
+++ PHONETASTIK/src/main/java/fr/phonetastik/controller/AcceuilController.java
@@ -6,15 +6,21 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-
 @Controller
 public class AcceuilController {
-	
-	//mapping pour methode Get
-		@GetMapping("/home")
-		public String viewTemplate(Model model) {
-			//objet model permet d'inserer des attributs dans la vue et les recuperer
-			return "index";
-		}
+
+	// mapping pour methode Get
+	@GetMapping("/home")
+	public String viewTemplate(Model model) {
+		// objet model permet d'inserer des attributs dans la vue et les recuperer
+		return "index";
+	}
+
+	// mapping pour methode Get
+	@GetMapping("/acceuil")
+	public String viewTemplateAcceuil(Model model) {
+		// objet model permet d'inserer des attributs dans la vue et les recuperer
+		return "acceuil";
+	}
 
 }
